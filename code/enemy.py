894e0d10a37b54b854a76cad6553271c6e0b354c
Aleksus <sasha.rock.tank@gmail.com>
2025-05-05 17:56:10 +0300

    Enemie can now move

--- code/enemy.py
+++ code/enemy.py
@@ -1,4 +1,5 @@
 from settings import *
+from numpy import sign
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, surf, spawn_line, turn_lines, groups):
@@ -27,13 +28,7 @@ class Enemy(pygame.sprite.Sprite):
         self.turn_lines = turn_lines
 
         # set distance to next turn line
-        turn_line_points = self.turn_lines[self.next_line].points
-        bottom_center = self.rect.midbottom
-        self.distance = ((turn_line_points[1][0] - turn_line_points[0][0]) * (turn_line_points[0][1] - bottom_center[1]) -
-                            (turn_line_points[0][0] - bottom_center[0]) * (turn_line_points[1][1] - turn_line_points[0][1])
-                        ) / math.sqrt(pow(turn_line_points[1][0] - turn_line_points[0][0], 2) 
-                                        + pow(turn_line_points[1][1] - turn_line_points[0][1], 2))
-        print(self.distance)
+        self.set_next_line_distance()
 
         self.turned_lines = set()  # отслеживание уже использованных линий
 
@@ -41,38 +36,48 @@ class Enemy(pygame.sprite.Sprite):
         # Движение
         self.rect.center += self.direction * self.speed * dt
     
+    def set_next_line_distance(self):
+        turn_line_points = self.turn_lines[self.next_line].points
+        bottom_center = self.rect.midbottom
+        self.distance = ((turn_line_points[1][0] - turn_line_points[0][0]) * (turn_line_points[0][1] - bottom_center[1]) -
+                            (turn_line_points[0][0] - bottom_center[0]) * (turn_line_points[1][1] - turn_line_points[0][1])
+                        ) / math.sqrt(pow(turn_line_points[1][0] - turn_line_points[0][0], 2) 
+                                        + pow(turn_line_points[1][1] - turn_line_points[0][1], 2))
+    
     def make_a_turn(self):
             turn_line = self.turn_lines[self.next_line]
 
-            if turn_line[1] == "1" and self.direction.x == 1:           # шёл направо →
+            if turn_line.type == "1" and self.direction.x == 1:           # шёл направо →
                 self.direction = pygame.Vector2(0, 1)                # поворачивает вниз ↓
 
-            elif turn_line[1] == "1" and self.direction.x == -1:        # шёл налево ←
+            elif turn_line.type == "1" and self.direction.x == -1:        # шёл налево ←
                 self.direction = pygame.Vector2(0, -1)               # поворачивает вверх ↑
 
-            elif turn_line[1] == "1" and self.direction.y == 1:         # шёл вниз ↓
+            elif turn_line.type == "1" and self.direction.y == 1:         # шёл вниз ↓
                 self.direction = pygame.Vector2(1, 0)                # поворачивает направо →
 
-            elif turn_line[1] == "1" and self.direction.y == -1:        # шёл вверх ↑
+            elif turn_line.type == "1" and self.direction.y == -1:        # шёл вверх ↑
                 self.direction = pygame.Vector2(-1, 0)               # поворачивает налево ←
             
 
-            elif turn_line[1] == "2" and self.direction.x == 1:         # шёл направо →
+            elif turn_line.type == "2" and self.direction.x == 1:         # шёл направо →
                 self.direction = pygame.Vector2(0, -1)               # поворачивает вверх ↑
 
-            elif turn_line[1] == "2" and self.direction.x == -1:        # шёл налево ←
+            elif turn_line.type == "2" and self.direction.x == -1:        # шёл налево ←
                 self.direction = pygame.Vector2(0, 1)                # поворачивает вниз ↓
 
-            elif turn_line[1] == "2" and self.direction.y == 1:         # шёл вниз ↓
+            elif turn_line.type == "2" and self.direction.y == 1:         # шёл вниз ↓
                 self.direction = pygame.Vector2(-1, 0)               # поворачивает налево ←
 
-            elif turn_line[1] == "2" and self.direction.y == -1:        # шёл вверх ↑
+            elif turn_line.type == "2" and self.direction.y == -1:        # шёл вверх ↑
                 self.direction = pygame.Vector2(1, 0)                # поворачивает направо →
 
             self.turned_lines.add(self.next_line)
-            self.next_line =+ 1 
+            self.next_line = self.next_line + 1
+            self.set_next_line_distance()
 
     def check_turn(self):
+        print(self.next_line, self.turned_lines)
         if self.next_line not in self.turned_lines:
             turn_line = self.turn_lines[self.next_line]
             bottom_center = self.rect.midbottom
@@ -82,6 +87,8 @@ class Enemy(pygame.sprite.Sprite):
                             (turn_line_points[0][0] - bottom_center[0]) * (turn_line_points[1][1] - turn_line_points[0][1])
                         ) / math.sqrt(pow(turn_line_points[1][0] - turn_line_points[0][0], 2) 
                                         + pow(turn_line_points[1][1] - turn_line_points[0][1], 2))
+            if sign(self.distance) != sign(distance):
+                return True
             return False
 
     def update(self, dt):
