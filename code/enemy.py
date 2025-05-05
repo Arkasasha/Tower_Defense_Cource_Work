53ea5fc2d45b226fc8a01ff9b5e72287e32b58c2
Aleksus <sasha.rock.tank@gmail.com>
2025-05-05 16:36:43 +0300

    Reorganized the methods

--- code/enemy.py
+++ code/enemy.py
@@ -12,97 +12,80 @@ class Enemy(pygame.sprite.Sprite):
         spawn_y = random.randint(round(start_points[1]), round(end_points[1]))
         
         self.image = surf
-        self.rect = pygame.FRect(0, 0, 16, 16)
-        self.rect.center = (spawn_x, spawn_y)
+        # self.rect = pygame.FRect(0, 0, 16, 16)
+        # self.rect.midbottom = (spawn_x, spawn_y)
+        self.rect = self.image.get_frect(centerx = spawn_x, bottom = spawn_y)
+        
 
         self.hitbox = pygame.FRect(self.rect)
-
-        self.pos = pygame.Vector2(self.rect.centerx, self.rect.bottom)
         
         self.direction = pygame.Vector2(1, 0) # стартовое направление
         self.speed = 50
-        self.next_type = 1
-        self.mark = True
 
+        # set lines
+        self.next_line = 0
         self.turn_lines = turn_lines
+
+        # set distance to next turn line
+        turn_line_points = self.turn_lines[self.next_line].points
+        bottom_center = self.rect.midbottom
+        self.distance = ((turn_line_points[1][0] - turn_line_points[0][0]) * (turn_line_points[0][1] - bottom_center[1]) -
+                            (turn_line_points[0][0] - bottom_center[0]) * (turn_line_points[1][1] - turn_line_points[0][1])
+                        ) / math.sqrt(pow(turn_line_points[1][0] - turn_line_points[0][0], 2) 
+                                        + pow(turn_line_points[1][1] - turn_line_points[0][1], 2))
+        print(self.distance)
 
         self.turned_lines = set()  # отслеживание уже использованных линий
 
-    @staticmethod
-    def point_in_polygon(point, polygon):
-        # Чистая реализация ray casting алгоритма
-        x, y = point
-        inside = False
+    def move(self, dt):
+        # Движение
+        self.rect.center += self.direction * self.speed * dt
+    
+    def make_a_turn(self):
+            turn_line = self.turn_lines[self.next_line]
 
-        n = len(polygon)
-        px1, py1 = polygon[0]
-        for i in range(n + 1):
-            px2, py2 = polygon[i % n]
-            if y > min(py1, py2):
-                if y <= max(py1, py2):
-                    if x <= max(px1, px2):
-                        if py1 != py2:
-                            xinters = (y - py1) * (px2 - px1) / (py2 - py1) + px1
-                        if px1 == px2 or x <= xinters:
-                            inside = not inside
-            px1, py1 = px2, py2
+            if turn_line[1] == "1" and self.direction.x == 1:           # шёл направо →
+                self.direction = pygame.Vector2(0, 1)                # поворачивает вниз ↓
 
-        return inside
+            elif turn_line[1] == "1" and self.direction.x == -1:        # шёл налево ←
+                self.direction = pygame.Vector2(0, -1)               # поворачивает вверх ↑
+
+            elif turn_line[1] == "1" and self.direction.y == 1:         # шёл вниз ↓
+                self.direction = pygame.Vector2(1, 0)                # поворачивает направо →
+
+            elif turn_line[1] == "1" and self.direction.y == -1:        # шёл вверх ↑
+                self.direction = pygame.Vector2(-1, 0)               # поворачивает налево ←
+            
+
+            elif turn_line[1] == "2" and self.direction.x == 1:         # шёл направо →
+                self.direction = pygame.Vector2(0, -1)               # поворачивает вверх ↑
+
+            elif turn_line[1] == "2" and self.direction.x == -1:        # шёл налево ←
+                self.direction = pygame.Vector2(0, 1)                # поворачивает вниз ↓
+
+            elif turn_line[1] == "2" and self.direction.y == 1:         # шёл вниз ↓
+                self.direction = pygame.Vector2(-1, 0)               # поворачивает налево ←
+
+            elif turn_line[1] == "2" and self.direction.y == -1:        # шёл вверх ↑
+                self.direction = pygame.Vector2(1, 0)                # поворачивает направо →
+
+            self.turned_lines.add(self.next_line)
+            self.next_line =+ 1 
+
+    def check_turn(self):
+        if self.next_line not in self.turned_lines:
+            turn_line = self.turn_lines[self.next_line]
+            bottom_center = self.rect.midbottom
+
+            turn_line_points = turn_line.points    # two point: beginning and end
+            distance = ((turn_line_points[1][0] - turn_line_points[0][0]) * (turn_line_points[0][1] - bottom_center[1]) -
+                            (turn_line_points[0][0] - bottom_center[0]) * (turn_line_points[1][1] - turn_line_points[0][1])
+                        ) / math.sqrt(pow(turn_line_points[1][0] - turn_line_points[0][0], 2) 
+                                        + pow(turn_line_points[1][1] - turn_line_points[0][1], 2))
+            return False
 
     def update(self, dt):
-        # Движение
-        self.pos += self.direction * self.speed * dt
-        self.rect.center = (round(self.pos.x), round(self.pos.y))
-
-        # print(self.pos)
-
-        bottom_center = self.rect.midbottom
-        distance = 0
-
-        # print(self.turn_lines)
-            # Повороты 
-
-        for turn_lines in self.turn_lines:
-            poly_id = id(turn_lines.points)
-            if poly_id in self.turned_lines:
-                continue
+        self.move(dt)
+        if self.check_turn():
+            self.make_a_turn()
             
-            if turn_lines.name == self.next_type:
-                if self.mark == True:
-                    turn_line_points = (turn_lines.points)
-                    distance = abs((turn_line_points[1][0] - turn_line_points[0][0]) * (turn_line_points[0][1] - bottom_center[1]) -
-                                    (turn_line_points[0][0] - bottom_center[0]) * (turn_line_points[1][1] - turn_line_points[0][1])
-                                ) / math.sqrt(pow(turn_line_points[1][0] - turn_line_points[0][0], 2) + pow(turn_line_points[1][1] - turn_line_points[0][1], 2))
-                    self.mark = False
-
-            turn_line = (turn_lines.name, turn_lines.type)
-
-            if distance < 0.1:
-                print("yes")
-                if turn_line[1] == "1" and self.direction.x == 1:           # шёл направо →
-                    self.direction = pygame.Vector2(0, 1)                # поворачивает вниз ↓
-
-                elif turn_line[1] == "1" and self.direction.x == -1:        # шёл налево ←
-                    self.direction = pygame.Vector2(0, -1)               # поворачивает вверх ↑
-
-                elif turn_line[1] == "1" and self.direction.y == 1:         # шёл вниз ↓
-                    self.direction = pygame.Vector2(1, 0)                # поворачивает направо →
-
-                elif turn_line[1] == "1" and self.direction.y == -1:        # шёл вверх ↑
-                    self.direction = pygame.Vector2(-1, 0)               # поворачивает налево ←
-                
-
-                elif turn_line[1] == "2" and self.direction.x == 1:         # шёл направо →
-                    self.direction = pygame.Vector2(0, -1)               # поворачивает вверх ↑
-
-                elif turn_line[1] == "2" and self.direction.x == -1:        # шёл налево ←
-                    self.direction = pygame.Vector2(0, 1)                # поворачивает вниз ↓
-
-                elif turn_line[1] == "2" and self.direction.y == 1:         # шёл вниз ↓
-                    self.direction = pygame.Vector2(-1, 0)               # поворачивает налево ←
-
-                elif turn_line[1] == "2" and self.direction.y == -1:        # шёл вверх ↑
-                    self.direction = pygame.Vector2(1, 0)                # поворачивает направо →
-
-                self.next_type =+1
-                self.turned_lines.add(poly_id)
